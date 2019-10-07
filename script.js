a4e0247df2ff8f0a332a72549f95279e011d0326
charlese5002 <56263457+charlese5002@users.noreply.github.com>
2019-10-07 10:28:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = Charlie Eydman
+let userName = Charlese5002;
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
